c54c3653646d191479888552d5c8e83b2589fd4f
ElectricZebra <nicholasregoli@gmail.com>
2019-06-25 17:13:45 -0700

    displaying db data

--- main.js
+++ main.js
@@ -1,28 +1,22 @@
 const html = require('html-template-tag');
 
-module.exports = (content) => html`<html>
-<head>
+module.exports = (content) => html`
+<html>
+  <head>
 <style>
 </style>
-  <script src='https://cdnjs.cloudflare.com/ajax/libs/axios/0.18.0/axios.js'></script>
-  <script src="https://unpkg.com/babel-standalone@6/babel.min.js"></script>
-  <script src="https://unpkg.com/react@16/umd/react.development.js" crossorigin></script>
-  <script src="https://unpkg.com/react-dom@16/umd/react-dom.development.js" crossorigin></script>
-  <script src='https://cdnjs.cloudflare.com/ajax/libs/react-router-dom/5.0.0/react-router-dom.js'></script>
-</head>
-<body>
-  <div id='root'></div>
-  <script type='text/babel'>
-    const root = document.querySelector('#root');
-    const { React } from 'React'
-
-
-    class App extneds React.component {
-
-    }
-
-    ReactDOM.render(<App />, root);
-  </script>
-</body>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/axios/0.18.0/axios.js'></script>
+    <script src="https://unpkg.com/babel-standalone@6/babel.min.js"></script>
+    <script src="https://unpkg.com/react@16/umd/react.development.js" crossorigin></script>
+    <script src="https://unpkg.com/react-dom@16/umd/react-dom.development.js" crossorigin></script>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/react-router-dom/5.0.0/react-router-dom.js'></script>
+  </head>
+  <body>
+    <div id='root'></div>
+    <script type='text/babel'>
+      const root = document.querySelector('#root');
+      ReactDOM.render(<hr />, root);
+    </script>
+  </body>
 </html>
 `;
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const { syncAndSeed, models } = require('./db');
 const { Inventory } = models;
-const path = require('path')
+const { main } = require('./main');
 
 syncAndSeed();
 
@@ -12,7 +12,88 @@ app.listen(port, ()=>console.log(`listening on port ${port}`))
 
 app.get('/', async(req, res, next) => {
   try {
-    await res.sendFile(path.join(__dirname+"/index.html"))
+    const data = await Inventory.findAll();
+    await res.send(`
+    <html>
+      <head>
+        <style>
+          a {
+            margin: 10px;
+            background-color: grey;
+          }
+        </style>
+        <script src='https://cdnjs.cloudflare.com/ajax/libs/axios/0.18.0/axios.js'></script>
+        <script src="https://unpkg.com/babel-standalone@6/babel.min.js"></script>
+        <script src="https://unpkg.com/react@16/umd/react.development.js" crossorigin></script>
+        <script src="https://unpkg.com/react-dom@16/umd/react-dom.development.js" crossorigin></script>
+        <script src='https://cdnjs.cloudflare.com/ajax/libs/react-router-dom/5.0.0/react-router-dom.js'></script>
+      </head>
+      <body>
+        <div id='root'></div>
+        <script type='text/babel'>
+          const root = document.querySelector('#root');
+          const { HashRouter, Route, Link } = ReactRouterDOM;
+
+
+          const Products = ({ inventory }) => {
+            console.log(inventory)
+            return (
+              <ul>
+                {
+                  inventory.map(item => <li key={item.id}>{item.item}                       <select defaultValue={ item.status }>
+                      {
+                        ['INSTOCK', 'BACKORDERED', 'DISCONTINUED'].map(status =>
+                          <option key={status}>{status}</option>)
+                      }
+                    </select>
+                    <br /> updated at: {item.updatedAt}
+
+                    </li>)
+                }
+              </ul>
+            )
+          }
+
+          class App extends React.Component {
+            constructor () {
+              super ();
+              this.state = {
+                inventory: []
+              }
+            };
+
+            async componentDidMount () {
+              try {
+                const response = await fetch ('http://localhost:3000/api/products')
+                const inventory = await response.json();
+                this.setState({inventory})
+              }
+              catch (err) {
+                console.log(err)
+              };
+            }
+
+            render() {
+              const { inventory } = this.state
+              console.log(inventory)
+              return (
+                <HashRouter>
+                  <h1>Acme Inventory</h1>
+                  <Link to='/'>All Products({inventory.length})</Link>
+                  <Link to='/instock'>INSTOCK</Link>
+                  <Link to='/backordered'>BACKORDERED</Link>
+                  <Link to='/discontinued'>DISCONTINUED</Link>
+                  <Route path='/' exact render={ () => <Products inventory={ inventory } />} />
+                </HashRouter>
+              )
+            }
+          };
+
+          ReactDOM.render(<App />, root);
+        </script>
+      </body>
+    </html>
+    `)
   }
   catch (ex) {
     next(ex)
@@ -21,7 +102,6 @@ app.get('/', async(req, res, next) => {
 
 app.get('/api/products', async (req, res, next) => {
   try {
-    console.log('////////// inside app.get')
     res.send(await Inventory.findAll());
   }
   catch (ex) {
